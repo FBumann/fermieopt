b3f10c8b86c39b526f3a83eab002ecd5537d1b33
FBumann <117816358+FBumann@users.noreply.github.com>
2025-01-29 17:10:18 +0100

    Make run_model.py wok

--- run_model.py
+++ run_model.py
@@ -1,23 +1,23 @@
 import logging
-from pathlib import Path
 
 from fermieopt.DistrictHeating import ExcelModel
 from fermieopt.excel_output import ExcelFcts, create_report_grouped, visualize_results
 
 logger = logging.getLogger('flixOpt')
 
-# Specify paths and solver_name
-excel_file_path = r'Template_Input.xlsx'  # path of excel input file
+########################################################################################################################
 
-solver_name = 'highs'  # Choose open source solver highs
-# solver_name = "gurobi"  # Choose commercial solver (Free academic licences). Much faster for large Models and storages
+excel_file_path = r'Template_Input.xlsx'    # path to the excel input file
+
+solver_name = 'highs'                       # Preinstalled open source solver (default). Might take a while...
+# solver_name = "gurobi"                    # Commercial solver (Free academic licences). Much faster...
 
 
-def main(solver_name: str, excel_file_path: str):
+def main(excel_file_path: str, solver_name: str = 'highs'):
     excel_model = ExcelModel(excel_file_path=excel_file_path)
-    excel_model.district_heating_system.final_model.visualize_network(False, controls=['physics'])
+    excel_model.final_model.visualize_network(False, controls=['physics'])
     excel_model.solve_model(solver_name=solver_name, gap_frac=0.0005, timelimit=2 * 3600)
-    excel_model.district_heating_system.final_model.visualize_network(
+    excel_model.final_model.visualize_network(
         f'{excel_model.final_directory}/{excel_model.calc_name}_network.html', controls=['physics']
     )
 
@@ -30,14 +30,14 @@ def main(solver_name: str, excel_file_path: str):
     for bus in calc_results.bus_results:
         create_report_grouped(
             calc_results,
-            path=Path(calc_results.folder) / f'{calc_results.name}__Report_{bus}.pdf',
+            path=calc_results.folder / f'{calc_results.name}__Report_{bus}.pdf',
             connected_to=bus,
         )
     # calculation_results_for_further_inspection = excel_model.load_results()
 
 
 if __name__ == '__main__':
-    main(solver_name, excel_file_path)
+    main(excel_file_path, solver_name)
 
 # optional: change values for gap_frac and timelimit
 """
